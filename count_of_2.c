3c860a2de8ee0242f81e4a84c34f1a74124fe132
Ching Chao <cchao@e1z2r1p1.42.us.org>
2019-06-16 20:40:56 -0700

    update

---
+++ count_of_2.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+/*
+** Count the numbers of two (2) that appears in the number n passed as argument.
+** Second argument keeps track of the current count state.
+** If a number has 2 or more digits, we have to split it into smaller digits.
+** We do this by using recursion.
+** If it's a single digit, we can check really quick because 2 == n. ;)
+*/
+
+static void	count_number(int n, int *count)
+{
+	if (n >= 10)
+	{
+		count_number(n / 10, count);
+		count_number(n % 10, count);
+		printf("\neee %d, %d", n, *count);
+	}
+	if (n == 2)
+	{
+		//printf("\nbbb %d, %d", n, *count);
+		*count += 1;
+	}
+	return ;
+}
+
+/*
+** Counts, for a given integer n, the number of two (2) that appear in all
+** the numbers between 0 and n (inclusive).
+*/
+
+int			count_of_2(int n)
+{
+	int		i = 1;
+	int		count = 0;
+
+	while (++i <= n)
+		count_number(i, &count);
+	return (count);
+}
+
+/*
+** Main function. Uncomment to test this file!
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+int			main(int argc, char *argv[])
+{
+	if (argc == 2)
+		printf("\naaa %i\n", count_of_2(atoi(argv[1])));
+	return (0);
+}
---
+++ find_pivot.c
@@ -0,0 +1,49 @@
+
+/*
+** Get the total sum of all the values in an int array.
+*/
+
+static int	get_sum(int *arr, int n)
+{
+	int		sum;
+
+	sum = 0;
+	for (int i = 0; i < n; i += 1)
+		sum += arr[i];
+	return (sum);
+}
+
+/*
+** Return the pivot index of an array, where the sum of the values on the
+** left is equal to the sum on the right.
+*/
+
+int			find_pivot(int *arr, int n)
+{
+	int		count = 0;
+	int 	pivot = -1;
+	int		sum = get_sum(arr, n);
+
+	for (int i = 0; (pivot == -1 && i < n); i += 1)
+	{
+		sum -= arr[i];
+		pivot = (count == sum) ? i : -1;
+		count += arr[i];
+	}
+	return (pivot);
+}
+
+/*
+** Main function. Uncomment to test this file!
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+int 		main(int argc, const char *argv[])
+{
+	int		*arr = malloc(sizeof(int) * (argc - 1));
+	for (int i = 0; (argc - 1) > i;  i += 1)
+		arr[i] = atoi(argv[i + 1]);
+	return (printf("Pivot: %d\n", find_pivot(arr, argc - 1)));
+}
+
